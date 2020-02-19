efcde21f5c0eb0916fd55b9e1e76bb7a197e65c4
Rafael Nunes <rafaelns.br@gmail.com>
2020-02-18 21:59:15 -0300

    Adicionando main em Java para facilitar compilação

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import view.CalculadoraView;
+import view.JanelaPrincipal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new JanelaPrincipal(new CalculadoraView());
+    }
+}
